af5d84f24c8815f833ba2b418d62c17d28fb1b21
mustang2015 <giangpham92@gmail.com>
2014-05-09 18:03:33 +0200

    8. Commit

--- TPE3/src/hsma/uib/ss14/tpe08/p3/NodeListImpl.java
+++ TPE3/src/hsma/uib/ss14/tpe08/p3/NodeListImpl.java
@@ -1,14 +1,65 @@
 package hsma.uib.ss14.tpe08.p3;
-
+import java.util.Iterator;
 import java.util.LinkedList; // Aufwand sparen
+
 /**
- * Implementierung der spezialisierten Version des Interface NodeList.
+ * Verwaltung von Listen
  * 
  * @author Giang Pham
  * @author Joshua Barsoum
  * @author Hunar Mawlod
  *
  */
-public class NodeListImpl {
+public class NodeListImpl<T> implements NodeList<T>{
+
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(Object e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(Object e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addFirst(Object e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T peekFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T pollFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 }
