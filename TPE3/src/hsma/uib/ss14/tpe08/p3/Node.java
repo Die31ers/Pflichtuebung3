b44e4bb7cff5bc4cb1f58ceabaa9e10c7b46aad3
mustang2015 <giangpham92@gmail.com>
2014-05-09 18:03:33 +0200

    test

---
+++ TPE3/src/hsma/uib/ss14/tpe08/p3/Node.java
@@ -0,0 +1,27 @@
+package hsma.uib.ss14.tpe08.p3;
+
+/**
+ * Generische Klasse Knoten
+ * 
+ * @author Giang Pham
+ * @author Joshua Barsoum
+ * @author Hunar Mawlod
+ *
+ */
+public class Node {
+	
+	public void addChild(){
+		
+	}
+	
+	public void getChildren(){
+		
+	}
+	public void getName(){
+		
+	}
+	
+	public void getValue(){
+		
+	}
+}
---
+++ TPE3/src/hsma/uib/ss14/tpe08/p3/SearchStrategy.java
@@ -0,0 +1,14 @@
+package hsma.uib.ss14.tpe08.p3;
+
+/**
+ * Die Interface für Such Strategien.
+ * 
+ * @author Giang Pham
+ * @author Joshua Barsoum
+ * @author Hunar Mawlod
+ *
+ */
+public interface SearchStrategy {
+	public void search();
+	public void getPath();
+}
