4395e1182b2111f4e8cf7cfbfe431f3fbffd19e4
kingj_000 <kingj_000@192.168.2.104>
2014-05-13 15:38:42 +0200

    TEST.......

---
+++ TPE3/src/JUnitTestklasse/Hauptprogramm.java
@@ -0,0 +1,22 @@
+package JUnitTestklasse;
+
+import java.awt.List;
+import java.util.LinkedList;
+
+import Graph_List_ListImpl.*;
+import Node.*;
+import SearchStrategy.SearchStrategy;
+
+public class Hauptprogramm {
+	public static void main(String[] args) {
+		String a = "Sido - Weihnachtszeit";
+		String b = "Swagboy 9000 - nerv ich alle in facebook";
+		String c = "Bushido - Lass mich allein";
+		LinkedList liste = new LinkedList();
+
+		liste.add(a);
+		liste.add(b);
+		liste.add(c);
+		System.out.println(liste.get(2));
+	}
+}
