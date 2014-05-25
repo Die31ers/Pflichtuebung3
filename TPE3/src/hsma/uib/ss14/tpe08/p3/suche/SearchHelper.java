e7f5a6070e90f39687014a600640e00e9cf10af8
mustang2015 <giangpham92@gmail.com>
2014-05-25 23:30:32 +0200

    FINALE

--- TPE3/src/hsma/uib/ss14/tpe08/p3/suche/SearchHelper.java
+++ TPE3/src/hsma/uib/ss14/tpe08/p3/suche/SearchHelper.java
@@ -4,27 +4,28 @@ import hsma.uib.ss14.tpe08.p3.Node;
 import hsma.uib.ss14.tpe08.p3.list.NodeListImpl;
 
 /**
- * Helferklasse, um die Speicherung des
- * letzten Pfades nicht doppelt anzulegen.
- *
- * @param <T> Dynamischer Typ des Helferklasse
+ * Helferklasse, um die Speicherung des letzten Pfades nicht doppelt anzulegen.
+ * 
+ * @author Giang Pham
+ * @author Joshua Barsoum
+ * @author Hunar Mawlod
+ * 
+ * @param <T>
+ *            Dynamischer Typ des Helferklasse
  */
 public abstract class SearchHelper<T> {
-    /**
-     * Dynamische Liste zur Speicherung des
-     * letzten Pfades.
-     */
-    private NodeListImpl<Node<T>>
-            letzterPfad = new NodeListImpl<Node<T>>();
+	/**
+	 * Dynamische Liste zur Speicherung des letzten Pfades.
+	 */
+	private NodeListImpl<Node<T>> letzterPfad = new NodeListImpl<Node<T>>();
 
-    /**
-     * Methode um den letzten Pfad, welchen die Suche
-     * genommen hat auszugeben.
-     *
-     * @return Die Dynamische Liste
-     */
-    public NodeListImpl<Node<T>> getPath() {
-        return letzterPfad;
-    }
+	/**
+	 * Methode um den letzten Pfad, welchen die Suche genommen hat auszugeben.
+	 * 
+	 * @return Die Dynamische Liste
+	 */
+	public NodeListImpl<Node<T>> getPath() {
+		return letzterPfad;
+	}
 
 }
